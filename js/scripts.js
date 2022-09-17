1c42109f5512f8d76e98188bfd1c0b34d0a4aa84
vitorialss <107154247+vitorialss@users.noreply.github.com>
2022-09-17 15:43:06 -0300

    "primeira versão do projeto finalizada"

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+
+const getLettersLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLettersUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumbers = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbols = () => {
+    const symbols = "(){}[]=<>/.,!?$%@#&*+-";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+
+};
+
+const generatePassword = (getLettersLowerCase, getLettersUpperCase, getNumbers, getSymbols) => {
+    let password = "";
+
+    const passwordLength = 12;
+
+    const generators = [
+        getLettersLowerCase,
+        getLettersUpperCase,
+        getNumbers,
+        getSymbols
+    ];
+
+    for (i = 0; i < passwordLength; i = i + generators.length) {
+        generators.forEach(() => {
+            const randomValue = generators[Math.floor(Math.random() * generators.length)]();
+
+            password += randomValue;
+        })
+    }
+
+
+    generatedPasswordElement.style.display = "block";
+    generatedPasswordElement.querySelector("h4").innerText = password;
+}
+
+//Events
+generatePasswordButton.addEventListener("click", () => {
+    generatePassword(
+        getLettersLowerCase,
+        getLettersUpperCase,
+        getNumbers,
+        getSymbols);
+});
